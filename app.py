e3d9c5b0276eae2fbe0ac592fd988f54af3158e8
Alessandro Convertino <65157860+alesco1973@users.noreply.github.com>
2024-11-13 15:22:09 +0000

    commit app.py update 2

--- app.py
+++ app.py
@@ -279,7 +279,7 @@ def get_mister_info(username, mister_data):
     return None
 
 repo_url = "https://github.com/alesco1973/ASA_SGS.git"
-local_dir = "./asa_sgs"
+local_dir = "C:/asa_sgs"
 repo = access_repository(repo_url, local_dir)
 
 
