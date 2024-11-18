d6d3e9d40663f6f7e7e8eba67ccbb75b2269b1cf
alissio1973 <alessandro.convertino@live.it>
2024-11-18 09:13:00 +0000

    commit app.py 7

--- app.py
+++ app.py
@@ -327,50 +327,14 @@ def get_mister_info(username, mister_data):
             return allenatore
     return None
 
-repo_path = "https://github.com/alesco1973/ASA_SGS.git"
+#repo_url = "https://github.com/alesco1973/ASA_SGS.git"
 local_dir = "./asa_sgs"
 credentials = load_credentials('config.json')
 token = "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"
 repo_url = f"https://{token}@github.com/alesco1973/ASA_SGS.git"
-credentials_content = f"https://<{credentials['username']}<:<{credentials['password']}<@github.com"
-
-# Funzione per creare il file .git-credentials
-def create_git_credentials(p_repo):
-    try:
-        # Cambia directory alla repository locale
-        os.chdir(p_repo)
-        
-        # Crea il file .git-credentials con le credenziali
-        with open(credentials_path, 'w') as file:
-            file.write(credentials_content)
-        
-        # Aggiungi il file alla repository
-        subprocess.run(["git", "add", credentials_path], check=True)
-        
-        # Commit delle modifiche
-        subprocess.run(["git", "commit", "-m", "Aggiungi file .git-credentials"], check=True)
-        
-        # Pusha le modifiche al repository remoto
-        subprocess.run(["git", "push", "origin", "main"], check=True)
-        
-        st.text("File .git-credentials creato e pushato con successo.")
-    except Exception as e:
-        st.text(f"Si è verificato un errore: {e}")
 
 # Percorso assoluto per il file .git-credentials
 credentials_path = os.path.expanduser("~/.git-credentials")
-# Verifica se il file esiste
-if os.path.exists(credentials_path):
-    try:
-        with open(credentials_path, 'r') as file:
-            credit = file.read()
-            st.text(credit)
-    except Exception as e:
-        st.text(f"Si è verificato un errore durante la lettura del file: {e}")
-else:
-    create_git_credentials(repo_path)
-
-
 # usr = credentials['username']
 # pwd = credentials['password']
 repo = access_repository(repo_url, local_dir)
