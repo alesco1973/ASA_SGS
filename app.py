1e2d8835f595b6c8db19660cb4033431d852fa08
Alessandro Convertino <65157860+alesco1973@users.noreply.github.com>
2024-11-21 07:50:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1021,9 +1021,13 @@ def gestione_rosa():
                             st.bar_chart(chart_data, x="giocatore", y=["presenze", "titolare", "sub_out", "sub_in"], x_label="Informazioni presenze", color="partite", horizontal=True)
                         elif column == 'minuti giocati/convocazioni':
                             st.warning("⬅️ Visualizza il rapporto tra i minuti giocabili, basato sul numero di partite in cui è stato convocato, e i minuti effettivi giocati.")
-                            convocazioni = df['partite'] - df['non convocazione']
-                            minutaggio = minuti*convocazioni
-                            df['rapporto'] = (df['minuti giocati'])*100/minutaggio
+                            if df['presenze'] != 0:
+                                convocazioni = df['partite'] - df['non convocazione']
+                                minutaggio = minuti*convocazioni
+                                df['rapporto'] = (df['minuti giocati'])*100/minutaggio
+                            else:
+                                df['rapporto'] = 0
+                                
                             chart_data = pd.DataFrame(
                                 {
                                     "giocatore": df['giocatore'],
